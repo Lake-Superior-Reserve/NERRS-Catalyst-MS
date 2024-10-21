1f237a5529b84d6b4479736c2877d8546dd80d2f
Daniel Szydlowski <dszydlowski@wisc.edu>
2024-10-21 13:43:41 -0500

    identifying heatwave events across the NERR

---
+++ Heatwaves scripts/format data for heatwaves.R
@@ -0,0 +1,51 @@
+# identify which years have enough years (>=10) to be used in 
+# heatwave analysis
+
+# DK Szydlowski 2024-10-20
+library(tidyverse)
+
+# list all of the daily data files
+
+daily.data.files = list.files("./Catalyst Project Data/QAQCd_daily/", full.names = TRUE)
+
+# filter to just water quality data, which has water temperature
+daily.data.files = daily.data.files[grepl("wq", daily.data.files)]
+
+# loop through and identify which files have at least 10 years of data
+for(i in 1:length(daily.data.files)){
+  
+  # read in each csv
+  temp = read.csv(daily.data.files[i])
+  
+  # format date and create a year column
+  temp = temp %>% 
+    mutate(date = ymd(date)) %>% 
+    mutate(year = year(date)) 
+
+  print(max(temp$year, na.rm = TRUE) - min(temp$year, na.rm = TRUE))
+
+  
+  # if there are enough years, keep the data
+  if(max(temp$year, na.rm = TRUE) - min(temp$year, na.rm = TRUE) >= 10){
+    
+    temp = temp %>% select(station, date, year, temp_nValid, temp_min, temp_median, temp_max,
+                           temp_mean, temp_sd, temp_iqr)
+    
+    if( i == 1){
+      all.temp = temp
+    }
+    if(i > 1){
+      all.temp = rbind(all.temp, temp)
+    }
+    
+  }
+  
+}
+  
+
+
+# create a site and station column
+all.temp = all.temp %>% mutate(site = substr(station, 1, 3)) %>% 
+  mutate(station = substr(station, 4, 5))
+
+# there are 17 NERR's with sufficient data (gaps notwithstanding) to identify heatwaves
---
+++ Heatwaves scripts/identify heatwaves.R
@@ -0,0 +1,68 @@
+# identify heatwaves from the previously identified sites
+
+library(heatwaveR)
+
+# create a new column which has site_station
+all.temp = all.temp %>% mutate(site_station = paste(site, station, sep = "_"))
+
+site_stations = unique(all.temp$site_station)
+
+i = 1
+for(i in 1:length(site_stations)){
+  
+  cur.site.station = site_stations[i]
+  cur.all.temp = all.temp %>% filter(site_station == cur.site.station)
+  
+  # format for heatwaveR
+  cur.all.temp = cur.all.temp %>% rename(t = date, temp = temp_mean) %>% 
+    select(t, temp)
+  
+  climOutput = ts2clm(cur.all.temp, climatologyPeriod = c(min(cur.all.temp$t), max(cur.all.temp$t)))
+  hwOutput = detect_event(climOutput)$event
+  
+  store = detect_event(climOutput)
+  
+  hwOutput$site_station = cur.site.station
+  
+  if(i == 1){
+    hw.all = hwOutput
+  }
+  if(i > 1){
+    hw.all = rbind(hw.all, hwOutput)
+  }
+  
+}
+
+
+## there are 3000 heatwaves
+# average length is 8.5 days
+
+hw.all = hw.all %>% select(site_station, everything())
+
+hw.stats = hw.all %>% 
+  group_by(site_station) %>% 
+  summarize(n.hw = n(), hw.duration = mean(duration, na.rm= TRUE)) 
+
+hw.stats = hw.stats %>% mutate(site = substr(site_station, 1, 3))
+
+
+ggplot(hw.stats, aes(x = site, y = n.hw, fill = site))+
+  geom_boxplot()+
+  theme_classic()
+
+
+ggplot(hw.stats %>% filter(site != "kac"), aes(x = site, y = hw.duration, fill = site))+
+  geom_boxplot()+
+  theme_classic()
+
+
+ggplot(hw.all, aes(x = duration)) +
+  geom_histogram( fill = "darkred", color = "black") +
+  labs(title = "", x = "Duration of heatwaves", y = "Frequency") +
+  theme_minimal()+
+  scale_x_log10(labels = scales::comma) 
+  
+
+min.date = min(store$climatology$t)
+max.date = max(store$climatology$t)
+event_line(store, spread = 300, category = TRUE)
